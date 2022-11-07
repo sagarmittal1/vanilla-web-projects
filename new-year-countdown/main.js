80f73fde59c9f8a2634846aa76070892d0c6e875
Sagar Mittal <mittalsagar006@gmail.com>
2022-11-08 02:34:05 +0530

    made new year countdown app

---
+++ new-year-countdown/main.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const countdown = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const newYearDate = new Date(currentYear + 1, 0, 1);
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 86400);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  // console.log(days, hours, minutes, seconds);
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+};
+
+const formatTime = (time) => {
+  return time < 10 ? `0${time}` : time; 
+}
+
+countdown();
+
+setInterval(countdown, 1000);
